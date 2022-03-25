0ea2a0aaf163f1aafd81334b7acb36cf63c7084f
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2022-03-25 16:31:06 +0200

    Remove all-zero columns

--- src/additive_models_with_optimal_features_transformations.py
+++ src/additive_models_with_optimal_features_transformations.py
@@ -13,11 +13,13 @@ from sklearn.model_selection import KFold
 from scipy.special import expit
 
 class OptimalFeaturesTransforamtionAdditiveModel:
-    def __init__(self, lambda_param, number_of_points=10, balance=1.0):
+    def __init__(self, lambda_param, number_of_points=10, balance=1.0, remove_all_zero_columns=True):
         self.limits = np.linspace(0.0, 1.0, num=number_of_points, endpoint=False)
         self.div = 1.0/float(number_of_points)
         self.lambda_param = lambda_param
         self.balance = balance
+        self.remove_all_zero_columns = remove_all_zero_columns
+        self.zero_columns_indices = []
 
     @staticmethod
     def __objective_function(beta,X_train_work, balanced_Y_train_work, lambda_param):
@@ -41,9 +43,15 @@ class OptimalFeaturesTransforamtionAdditiveModel:
                 X_j[(X_j>=limit) & (X_j<limit + self.div)] = 1.0
                 X_j[X_j<1.0] = 0.0
                 X_train_work[:,j*m:(j+1)*m] = X_j
+        # Remove all-zeros columns
+        if self.remove_all_zero_columns: 
+            self.zero_columns_indices = np.where(~X_train_work.any(axis=0))[0]
+            X_train_work = np.delete(X_train_work, self.zero_columns_indices, axis=1)
+            print(f'{len(self.zero_columns_indices)} columns were removed because they had only zeros.')
         bias_col = np.ones((n,1))
+        # Add bias
         X_train_work = np.append(X_train_work, bias_col, axis=1)
-        beta = np.zeros(m*j_max+1)
+        beta = np.zeros(X_train_work.shape[1])
         #beta = np.random.rand(m*j_max)
         if self.balance != 1.0:
             balanced_Y_train_work = np.array([self.balance * y if y > 0.0 else y for y in Y_train_work])
@@ -63,6 +71,9 @@ class OptimalFeaturesTransforamtionAdditiveModel:
             X_j[X_j<1.0] = 0.0
             X_transformed[:,j*m:(j+1)*m] = X_j
         n = X.shape[0]
+        # Remove all-zeros columns
+        if self.remove_all_zero_columns:
+            X_transformed = np.delete(X_transformed, self.zero_columns_indices, axis=1)
         bias_col = np.ones((n,1))
         X_transformed = np.append(X_transformed, bias_col, axis=1)
         predictions = X_transformed.dot(self.beta_optim)
@@ -79,16 +90,14 @@ class OptimalFeaturesTransforamtionAdditiveModel:
     
     def evaluate_k_fold(self, X, Y, n_splits):
         # Setup
-        kf = KFold(n_splits=n_splits, shuffle=True,random_state=100)
         test_accuracy_list = list()
         training_accuracy_list = list()
         test_auc_list = list()
         training_auc_list = list()
         training_time_list = list()
-        for train_index, test_index in kf.split(X):
+        for test_fold_index in range(n_splits):
             print("Process new folds...")
-            X_train, X_test = X[train_index], X[test_index]
-            Y_train, Y_test = Y[train_index], Y[test_index]
+            (X_train, Y_train), (X_test, Y_test) = data_utils.get_train_test_fold(X,Y,test_fold_index+1,n_splits)
             start = time.time()
             beta_optim = self.train(X_train,Y_train)
             end = time.time()
@@ -131,67 +140,24 @@ def compas_k_fold_test():
     regularisation_param = 0.01
     number_of_points = 50
     oftam = OptimalFeaturesTransforamtionAdditiveModel(regularisation_param, number_of_points)
-    # Setup
+    # Evaluate
     n_splits = 5
-    kf = KFold(n_splits=n_splits, shuffle=True,random_state=100)
-    test_accuracy_avg = 0.0
-    training_accuracy_avg = 0.0
-    test_auc_avg = 0.0
-    training_auc_avg = 0.0
-    for train_index, test_index in kf.split(X):
-        print("Process new folds...")
-        X_train, X_test = X[train_index], X[test_index]
-        Y_train, Y_test = Y[train_index], Y[test_index]
-        beta_optim = oftam.train(X_train,Y_train)
-        accuracy, auc = oftam.evaluate(X_train, Y_train)
-        training_accuracy_avg += accuracy
-        training_auc_avg += auc
-        print('Training accuracy:', accuracy)
-        print('Training AUC score:', auc)
-        accuracy, auc = oftam.evaluate(X_test, Y_test)
-        test_accuracy_avg += accuracy
-        test_auc_avg += auc
-        print('Test accuracy:', accuracy)
-        print('Test AUC score:', auc)
-    print('Average training accuracy:', training_accuracy_avg/float(n_splits))
-    print('Average training AUC score:', training_auc_avg/float(n_splits))
-    print('Average test accuracy:', test_accuracy_avg/float(n_splits))
-    print('Average test AUC score:', test_auc_avg/float(n_splits))
+    oftam.evaluate_k_fold(X, Y, n_splits)
 
-def compas_k_fold_test_nam():
+def credit_k_fold_test():
     # Data 
-    comps_data = pd.read_csv('./data/compas.csv', delimiter=';')
-    normalized_comps_data=(comps_data-comps_data.min())/(comps_data.max()-comps_data.min())
-    Y = normalized_comps_data['two_year_recid'].to_numpy()
-    X = normalized_comps_data.loc[:, normalized_comps_data.columns != 'two_year_recid'].to_numpy()
+    credit_data = pd.read_csv('./data/credit.csv')
+    credit_data.drop(['No', 'Time'], axis = 1, inplace = True)
+    normalized_credit_data=(credit_data-credit_data.min())/(credit_data.max()-credit_data.min())
+    Y = normalized_credit_data['Class'].to_numpy()
+    X = normalized_credit_data.loc[:,normalized_credit_data.columns != 'Class'].to_numpy()
     # Model
-    regularisation_param = 0.01
-    number_of_points = 55
+    regularisation_param = 0.005
+    number_of_points = 10
     oftam = OptimalFeaturesTransforamtionAdditiveModel(regularisation_param, number_of_points)
-    # Setup
+    # Evaluate
     n_splits = 5
-    test_accuracy_avg = 0.0
-    training_accuracy_avg = 0.0
-    test_auc_avg = 0.0
-    training_auc_avg = 0.0
-    for test_fold_index in range(n_splits):
-        print("Process new folds...")
-        (X_train, Y_train), (X_test, Y_test) = data_utils.get_train_test_fold(X,Y,test_fold_index+1,n_splits)
-        beta_optim = oftam.train(X_train,Y_train)
-        accuracy, auc = oftam.evaluate(X_train, Y_train)
-        training_accuracy_avg += accuracy
-        training_auc_avg += auc
-        print('Training accuracy:', accuracy)
-        print('Training AUC score:', auc)
-        accuracy, auc = oftam.evaluate(X_test, Y_test)
-        test_accuracy_avg += accuracy
-        test_auc_avg += auc
-        print('Test accuracy:', accuracy)
-        print('Test AUC score:', auc)
-    print('Average training accuracy:', training_accuracy_avg/float(n_splits))
-    print('Average training AUC score:', training_auc_avg/float(n_splits))
-    print('Average test accuracy:', test_accuracy_avg/float(n_splits))
-    print('Average test AUC score:', test_auc_avg/float(n_splits))
+    oftam.evaluate_k_fold(X, Y, n_splits)
 
 if __name__ == '__main__':
-    compas_k_fold_test_nam()
+    credit_k_fold_test()
