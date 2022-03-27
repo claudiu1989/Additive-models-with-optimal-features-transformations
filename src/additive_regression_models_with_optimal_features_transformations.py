0c5563ab3277222b7f9af469a2f9247f2d626084
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2022-03-27 18:17:50 +0300

    Remove normalisation of y, add linear regression baseline

--- src/additive_regression_models_with_optimal_features_transformations.py
+++ src/additive_regression_models_with_optimal_features_transformations.py
@@ -9,6 +9,8 @@ import data_utils
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 
+from sklearn.linear_model import LinearRegression
+
 class OptimalFeaturesTransforamtionAdditiveModel:
     def __init__(self, lambda_param, number_of_points=10, remove_all_zero_columns=True):
         self.limits = np.linspace(0.0, 1.0, num=number_of_points, endpoint=False)
@@ -21,8 +23,9 @@ class OptimalFeaturesTransforamtionAdditiveModel:
     def __objective_function(beta,X_train_work, Y_train_work, lambda_param):
         n = X_train_work.shape[0]
         current_predictions = X_train_work.dot(beta)
-        empirical_risk = (1.0/float(n))*np.linalg.norm(current_predictions - Y_train_work,ord=2) 
-        obj_value = empirical_risk + lambda_param*np.linalg.norm(beta,ord=2)
+        #empirical_risk = (1.0/float(n))*np.linalg.norm(current_predictions - Y_train_work,ord=2) 
+        empirical_risk = np.sqrt((np.sum((current_predictions- Y_train_work)**2))/float(n))
+        obj_value = empirical_risk + lambda_param*np.linalg.norm(beta,ord=1)
         #print(obj_value)
         return obj_value
 
@@ -45,6 +48,8 @@ class OptimalFeaturesTransforamtionAdditiveModel:
         bias_col = np.ones((n,1))
         # Add bias
         X_train_work = np.append(X_train_work, bias_col, axis=1)
+        # Add original features
+        X_train_work = np.append(X_train_work, X_train, axis=1)
         beta = np.zeros(X_train_work.shape[1])
         #beta = np.random.rand(m*j_max)
         result = opt.minimize(OptimalFeaturesTransforamtionAdditiveModel.__objective_function,  beta, args=(X_train_work, Y_train, self.lambda_param),  tol=1e-3)
@@ -66,12 +71,18 @@ class OptimalFeaturesTransforamtionAdditiveModel:
             X_transformed = np.delete(X_transformed, self.zero_columns_indices, axis=1)
         bias_col = np.ones((n,1))
         X_transformed = np.append(X_transformed, bias_col, axis=1)
+        # Add original features
+        X_transformed = np.append(X_transformed, X, axis=1)
         predictions = X_transformed.dot(self.beta_optim)
         return predictions
 
     def evaluate(self, X_test, Y_test):
         Y_predicted = self.predict(X_test)
         rmse = mean_squared_error(Y_test, Y_predicted, squared=False)
+        #print(Y_test[:20])
+        #print(Y_predicted[:20])
+        #print(np.sqrt((np.sum((Y_predicted- Y_test)**2))/float(len(Y_predicted))))
+        #print(rmse)
         return rmse
     
     def grid_search(self, X_train_validation, Y_train_validation, n_splits, lambda_values, number_of_points_values):
@@ -154,16 +165,58 @@ def housing_k_fold_test():
     # Data 
     housing_data = pd.read_csv('./data/california_housing.csv')
     housing_data.drop(['No'], axis = 1, inplace = True)
-    normalized_comps_data=(housing_data-housing_data.min())/(housing_data.max()-housing_data.min())
-    Y = normalized_comps_data['Label'].to_numpy()
-    X = normalized_comps_data.loc[:, normalized_comps_data.columns != 'Label'].to_numpy()
+    normalized_housing_data=(housing_data-housing_data.min())/(housing_data.max()-housing_data.min())
+    Y = housing_data['Label'].to_numpy()
+    X = normalized_housing_data.loc[:, normalized_housing_data.columns != 'Label'].to_numpy()
     # Model
-    regularisation_param = 0.01
-    number_of_points = 60
+    regularisation_param = 0.001
+    number_of_points = 30
     oftam = OptimalFeaturesTransforamtionAdditiveModel(regularisation_param, number_of_points)
     # Evaluate
     n_splits = 5
     oftam.evaluate_k_fold(X, Y, n_splits)
+
+def fico_k_fold_test():
+    # Data 
+    fico_data = pd.read_csv('./data/fico.csv')
+    fico_data.drop(['No'], axis = 1, inplace = True)
+    normalized_fico_data=(fico_data-fico_data.min())/(fico_data.max()-fico_data.min())
+    Y = fico_data['ExternalRiskEstimate'].to_numpy()
+    X = normalized_fico_data.loc[:, normalized_fico_data.columns != 'ExternalRiskEstimate'].to_numpy()
+    # Model
+    regularisation_param = 0.01
+    number_of_points = 10
+    oftam = OptimalFeaturesTransforamtionAdditiveModel(regularisation_param, number_of_points)
+    # Evaluate
+    n_splits = 5
+    oftam.evaluate_k_fold(X, Y, n_splits)
+
+def predict_linear_regression_fico():
+    fico_data = pd.read_csv('./data/fico.csv')
+    fico_data.drop(['No'], axis = 1, inplace = True)
+    normalized_fico_data=(fico_data-fico_data.min())/(fico_data.max()-fico_data.min())
+    Y = fico_data['ExternalRiskEstimate'].to_numpy()
+    X = normalized_fico_data.loc[:, normalized_fico_data.columns != 'ExternalRiskEstimate'].to_numpy()
+    test_rmse_list = list()
+    training_time_list = list()
+    n_splits = 5
+    for test_fold_index in range(n_splits):
+        print("Process new folds...")
+        (X_train_validation, Y_train_validation), (X_test, Y_test) = data_utils.get_train_test_fold(X,Y,test_fold_index+1,n_splits)
+        start = time.time()
+        reg = LinearRegression().fit(X_train_validation,Y_train_validation)
+        end = time.time()
+        Y_redicted = reg.predict(X_test)
+        rmse = mean_squared_error(Y_test, Y_redicted, squared=False)
+        print('Test RMSE:', rmse)
+        test_rmse_list.append(rmse)
+    avg_test_rmse = sum(test_rmse_list)/float(n_splits)
+    std_test_rmse = np.sqrt((np.sum((np.array(test_rmse_list) - avg_test_rmse)**2))/float(n_splits))
+    print('Average test RMSE: ', avg_test_rmse)
+    print('Standard deviation of test RMSE:', std_test_rmse)
+    print('Training time (s): ', sum(training_time_list)/float(n_splits))
     
 if __name__ == '__main__':
    housing_k_fold_test()
+   #fico_k_fold_test()
+   #predict_linear_regression_fico()
